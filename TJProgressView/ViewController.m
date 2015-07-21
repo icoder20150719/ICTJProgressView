857a0076042ac5583c015d75983bfa81f6f09b8b
ebaotong <2455063164@qq.com>
2015-07-21 13:40:17 +0800

    动画进度条

---
+++ TJProgressView/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  TJProgressView
+//
+//  Created by MJ on 15/7/20.
+//  Copyright (c) 2015年 TJ. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ICTJProgressView.h"
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet ICTJProgressView *progressView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    _progressView.progressColor = [UIColor colorWithRed:0.590 green:0.208 blue:0.041 alpha:1.000];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btnClick:(UIButton *)sender {
+    _progressView.progress =0.67;
+}
+
+@end
